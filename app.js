19782b10ea987a433725a7df54c90622ec16a36a
kuma0416 <a24350845@gmail.com>
2019-09-19 01:35:29 +0800

    'theFirstTry'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var fs = require('fs');
 var session = require('express-session');
 var db = require('./db');
